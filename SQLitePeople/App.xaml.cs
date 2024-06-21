d1d9f63f505e33b1a702ff8c9a7b8b263ad1e1a1
aguaman123 <87582447+aguaman123@users.noreply.github.com>
2024-06-20 20:09:40 -0500

    Agregar archivos de proyecto.

---
+++ SQLitePeople/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SQLitePeople
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
