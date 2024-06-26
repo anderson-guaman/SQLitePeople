d1857b48006e91cc5c664f47528473f3892eb445
Anderson Guaman <andersonguaman2002@gmail.com>
2024-06-26 08:58:53 -0500

    actualizacion del modelo person

--- SQLitePeople/Models/Person.cs
+++ SQLitePeople/Models/Person.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace SQLitePeople.Models
 {
+    [Table("people")]
     public class Person
     {
+        [PrimaryKey,AutoIncrement]
         public int IdPerson  {get;set;}
+        [MaxLength(250),Unique]
         public string Name { get; set; }
 
     }
