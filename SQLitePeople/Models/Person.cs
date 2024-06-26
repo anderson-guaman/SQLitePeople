c1b3dba4b3cc9cbb3b04e73d0a7ff471dc592169
Anderson Guaman <andersonguaman2002@gmail.com>
2024-06-26 08:54:54 -0500

    creado el objeto Person

---
+++ SQLitePeople/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLitePeople.Models
+{
+    public class Person
+    {
+        public int IdPerson  {get;set;}
+        public string Name { get; set; }
+
+    }
+}
